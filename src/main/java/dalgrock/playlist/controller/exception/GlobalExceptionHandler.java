a5d6d45731a5b91e5ca20edd176d76864c0f1c50
ppparkta <pppppark2@gmail.com>
2026-02-02 23:10:46 +0900

    refactor: 전역 예외처리 문자열 하드코딩 제거

--- src/main/java/dalgrock/playlist/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/dalgrock/playlist/controller/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package dalgrock.playlist.controller.exception;
 
 import dalgrock.playlist.core.exception.BusinessException;
 import dalgrock.playlist.core.exception.ErrorCode;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,22 +13,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Log log = LogFactory.getLog(GlobalExceptionHandler.class);
+
     @ExceptionHandler(BusinessException.class)
     protected ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
         ErrorCode errorCode = e.getErrorCode();
-        ErrorResponse response = ErrorResponse.of(errorCode.getCode(), e.getMessage());
+        ErrorResponse response = ErrorResponse.of(errorCode);
         return new ResponseEntity<>(response, errorCode.getStatus());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        ErrorResponse response = ErrorResponse.of("C001", "입력값이 유효하지 않습니다.");
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> handleException(Exception e) {
-        ErrorResponse response = ErrorResponse.of("S001", "서버 내부 오류가 발생했습니다.");
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
