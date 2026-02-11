f88f1b64eb224b7927d385e3d3f2dc1021ba58d3
ppparkta <pppppark2@gmail.com>
2026-02-11 21:29:13 +0900

    feat: 레포지토리 태그 추가

--- src/main/java/dalgrock/playlist/controller/RecordControllerV1.java
+++ src/main/java/dalgrock/playlist/controller/RecordControllerV1.java
@@ -4,6 +4,7 @@ import dalgrock.playlist.model.UserPrincipal;
 import dalgrock.playlist.service.RecordService;
 import dalgrock.playlist.service.dto.response.GetRecordDetailResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Record", description = "내 기록 관리 API")
 @SecurityRequirement(name = "cookieAuth")
 @RequiredArgsConstructor
 @RestController
