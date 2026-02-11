3bdd1f788dd4c24f133316f2fb569fd104987739
ppparkta <pppppark2@gmail.com>
2026-02-11 21:31:36 +0900

    feat: Async 기능 추가

--- src/main/java/dalgrock/playlist/PlaylistApplication.java
+++ src/main/java/dalgrock/playlist/PlaylistApplication.java
@@ -3,7 +3,9 @@ package dalgrock.playlist;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class PlaylistApplication {
 
