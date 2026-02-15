2b819e792cb161b93f5629f20ca0a3aada05abad
realwhyjay <realwhyjay@adenasoft.com>
2026-02-15 22:17:00 +0900

    fix: add configurable Swagger server URL to resolve mixed-content block

--- src/main/java/dalgrock/playlist/config/SwaggerConfig.java
+++ src/main/java/dalgrock/playlist/config/SwaggerConfig.java
@@ -4,18 +4,23 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.customizers.OperationCustomizer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${app.api.server-url:}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI openAPI() {
         String cookieAuthName = "cookieAuth";
 
-        return new OpenAPI()
+        OpenAPI openAPI = new OpenAPI()
                 .info(new Info()
                         .title("Pliview API")
                         .description("Pliview API 문서")
@@ -24,8 +29,13 @@ public class SwaggerConfig {
                         .addSecuritySchemes(cookieAuthName,
                                 new SecurityScheme()
                                         .name("access_token")
-                                        .type(SecurityScheme.Type.APIKEY)
-                                        .in(SecurityScheme.In.COOKIE)));
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.COOKIE)));
+
+        if (serverUrl != null && !serverUrl.isBlank()) {
+            openAPI.addServersItem(new Server().url(serverUrl).description("API Server"));
+        }
+        return openAPI;
     }
 
     @Bean
